314a000307e2989cddb800ac5dbb297abcfa8ac5
Luis Treto <luistreto@Air-de-Luis.attlocal.net>
2024-06-09 23:16:48 -0500

    Menu bar template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,20 @@
-<script>
-</script>
+<script></script>
 
 <template>
-<p>hello</p>
+    <div class="top-menu">
+        <div class="brand-logo">Branding name 🚀</div>
+        <ul class="menu-options">
+            <li>Help</li>
+            <li>Contact</li>
+            <li>Portfolio</li>
+        </ul>
+    </div>
 </template>
+<style>
+@import url("../assets/styles/flex-tests.scss");
+
+body {
+    color: rgb(55, 55, 55);
+    margin:0;
+}
+</style>
