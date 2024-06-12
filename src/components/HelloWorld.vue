60884cc13656cea5a5189402d1661d950a75ee46
Luis Treto <luistreto@Air-de-Luis.attlocal.net>
2024-06-12 15:11:22 -0500

    Menu flex responsive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,17 @@
-<script></script>
+<script>
+import TopMenu from "./TopMenu/TopMenu.vue";
+
+export default {
+    components: { TopMenu },
+};
+</script>
 
 <template>
-    <div class="top-menu">
-        <div class="brand-logo">Branding name 🚀</div>
-        <ul class="menu-options">
-            <li>Help</li>
-            <li>Contact</li>
-            <li>Portfolio</li>
-        </ul>
-    </div>
+    <TopMenu />
 </template>
 <style>
-@import url("../assets/styles/flex-tests.scss");
-
 body {
     color: rgb(55, 55, 55);
-    margin:0;
+    margin: 0;
 }
 </style>
---
+++ src/components/TopMenu/TopMenu.vue
@@ -0,0 +1,24 @@
+<script>
+
+
+</script>
+<template>
+    <div class="top-menu">
+        <div class="brand-logo">Brandingname</div>
+        <div class="menu-options">
+            <div>Help</div>
+            <div>Contact</div>
+            <div>Portfolio</div>
+        </div>
+        <div class="menu-toggle">
+            <img class="svg" src="../../assets/menu-dots.svg" alt="">
+            <!-- <svg class="svg" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 12H8.00901M12.0045 12H12.0135M15.991 12H16"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <circle cx="12" cy="12" r="10"  stroke-width="1.5"/>
+            </svg> -->
+        </div>
+    </div>
+</template>
+<style>
+@import url('./topMenu.scss');
+</style>
