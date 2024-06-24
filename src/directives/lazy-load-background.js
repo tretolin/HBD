af315c50c2c44c4eceb01b095d55311685492bd9
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2024-06-24 10:56:57 -0600

    lazy loaders in directives for image src and background-image

---
+++ src/directives/lazy-load-background.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyBackgrounds = [].slice.call(
+        document.querySelectorAll(".lazy-background")
+    );
+
+    if ("IntersectionObserver" in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function (
+            entries,
+            observer
+        ) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    console.log(entry.target, 'visible')
+                    entry.target.classList.add("visible");
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach(function (lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
+});
