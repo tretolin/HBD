e871a07917f4c84ed84cf77dfa7879b4a4efd4e4
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2024-06-24 13:13:20 -0600

    Project Item created with funcitonality

---
+++ src/components/ProjectItem/ProjectItem.vue
@@ -0,0 +1,26 @@
+<script setup>
+import "../../directives/lazy-load-background.js";
+import { defineProps } from "vue";
+
+const defaultImage = "https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE=";
+const props = defineProps({
+    image: {
+        type: Object,
+        required: false,
+        default: { url:defaultImage }
+    }
+});
+
+// const image = ref(urls[0].url).value;
+
+</script>
+
+<template>
+    <div class="image-container lazy-background"
+        :style="{ backgroundImage: 'url(' + image.url + ')' }">
+    </div>
+</template>
+
+<style>
+@import url('./ProjectItem.scss')
+</style>
