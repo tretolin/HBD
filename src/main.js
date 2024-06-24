2bfeaa869518b45ba5936ad76c58692bcb1e5f0f
Luis Treto <luistreto@MacBook-Air-de-Luis.local>
2024-06-24 10:55:19 -0600

    lazy-load integrated in main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import VueLazyload from "vue-lazyload";
+import lazyload from './directives/lazy-load';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.directive('lazyload', lazyload);
+app.mount("#app");
