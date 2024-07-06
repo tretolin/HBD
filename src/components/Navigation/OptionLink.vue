2c2b400dec18249c617335b7c5776ce710049ba2
Luis Treto <luisjavier.treto@gmail.com>
2024-07-06 15:19:11 -0600

    Links as a component

---
+++ src/components/Navigation/OptionLink.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps(["data"]);
+</script>
+<template>
+    <a class="navigation-link pointer"
+    :arial-label="props.data.arialLabel"
+    :href="props.data.href">
+        {{ props.data.title }}</a
+    >
+</template>
+<style>
+@import url('./OptionLink.scss');
+</style>
