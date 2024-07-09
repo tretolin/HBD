0351c8de318b3fe74fc77c180a80c33c8dc04881
Luis Treto <luisjavier.treto@gmail.com>
2024-07-08 23:05:28 -0600

    social media icons

---
+++ src/components/Profile/SocialMedia.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from "vue";
+
+const socialNetworks = ref([
+    {
+        name: "Facebook",
+        src: "facebook.svg",
+        url: "www.facebook.com",
+        active: true,
+    },
+    {
+        name: "Twitter",
+        src: "twitterx.svg",
+        url: "www.facebook.com",
+        active: true,
+    },
+    {
+        name: "Instagram",
+        src: "instagram.svg",
+        url: "www.facebook.com",
+        active: true,
+    },
+    {
+        name: "Whatsapp",
+        src: "whatsapp.svg",
+        url: "www.facebook.com",
+        active: true,
+    },
+    {
+        name: "Linkedin",
+        src: "linkedin.svg",
+        url: "www.facebook.com",
+        active: true,
+    },
+]);
+
+function asset(file) {
+    console.log("/src/assets/icons/" + file);
+    return "/src/assets/icons/" + file;
+}
+</script>
+<template>
+    <div class="social-media-container">
+        <div v-for="(network, index) in socialNetworks" class="network focused">
+            <a :href="network.url" :area-label="network.name" :key="index">
+                <img :src="asset(network.src)" :alt="network.name"/>
+            </a>
+        </div>
+    </div>
+</template>
+<style>
+.social-media-container {
+    display: flex;
+    gap:10px;
+    .network {
+        width: 6vw;
+    }
+}
+</style>
