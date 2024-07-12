d8510b1f8e2f716fbfa27c75690f3e2e8bdd05fa
Luis Treto <luisjavier.treto@gmail.com>
2024-07-11 19:21:41 -0600

    Use router in App

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import VueLazyload from "vue-lazyload";
-import lazyload from './directives/lazy-load';
+// import VueLazyload from "vue-lazyload";
+import lazyload from "./directives/lazy-load";
+import router from "./router";
 
-const app = createApp(App)
-app.directive('lazyload', lazyload);
+const app = createApp(App);
+app.use(router);
+app.directive("lazyload", lazyload);
 app.mount("#app");
