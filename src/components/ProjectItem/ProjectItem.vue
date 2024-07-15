b4a5454027206320e34bc0d1e8c176c529b868c9
Luis Treto <luisjavier.treto@gmail.com>
2024-07-15 17:33:49 -0600

    Fix for not showed images after navigation

--- src/components/ProjectItem/ProjectItem.vue
+++ src/components/ProjectItem/ProjectItem.vue
@@ -1,26 +1,38 @@
 <script setup>
-import "../../directives/lazy-load-background.js";
-import { defineProps } from "vue";
+import { ref } from "vue";
+import { defineProps, onMounted } from "vue";
 
-const defaultImage = "https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE=";
+let show = ref(false);
+
+const defaultImage =
+    "https://media.istockphoto.com/id/1222357475/vector/image-preview-icon-picture-placeholder-for-website-or-ui-ux-design-vector-illustration.jpg?s=612x612&w=0&k=20&c=KuCo-dRBYV7nz2gbk4J9w1WtTAgpTdznHu55W9FjimE=";
 const props = defineProps({
     image: {
         type: Object,
         required: false,
-        default: { url:defaultImage }
-    }
+        default: { url: defaultImage },
+    },
 });
 
-// const image = ref(urls[0].url).value;
+const showImage = () => (show.value = false);
 
+onMounted(() => {
+    setTimeout(() => {
+        show.value = true;
+    }, 500);
+
+    showImage();
+});
 </script>
 
 <template>
-    <div class="image-container lazy-background"
-        :style="{ backgroundImage: 'url(' + image.url + ')' }">
-    </div>
+    <div
+        class="image-container"
+        :class="['lazy-background', show ? 'visible' : '']"
+        :style="{ backgroundImage: 'url(' + image.url + ')' }"
+    ></div>
 </template>
 
 <style>
-@import url('./ProjectItem.scss')
+@import url("./ProjectItem.scss");
 </style>
