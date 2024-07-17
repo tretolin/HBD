7c6e7c1ca1162dcdc9349df1232f0656d52d1232
Luis Treto <luisjavier.treto@gmail.com>
2024-07-16 20:55:23 -0600

    Navigation adjustments

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -12,7 +12,6 @@ const menuOptions = ref([
 
 <template>
     <nav class="menu-options">
-        <a href=""></a>
         <OptionLink
             v-for="(link, index) in menuOptions"
             :data="link"
