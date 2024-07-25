d0c9d376a51e896073e02d68b8d43ada86c73232
Luis Treto <luisjavier.treto@gmail.com>
2024-07-24 19:09:59 -0600

    Fix Profile Load

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,8 +3,10 @@ import Profile from "../components/Profile/Profile.vue";
 import Gallery from "../components/Gallery/Gallery.vue";
 </script>
 <template>
-    <Profile />
-    <v-slot>
-        <Gallery />
-    </v-slot>
+    <div>
+        <Profile />
+        <v-slot>
+            <Gallery />
+        </v-slot>
+    </div>
 </template>
