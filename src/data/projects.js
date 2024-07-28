1020cc6df0c6e3f8921d8082153af794b8442d6d
Luis Treto <luisjavier.treto@gmail.com>
2024-07-28 13:56:45 -0600

    Imágenes de prueba

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,33 +2,46 @@ export const projects = [
     {
         id: 1,
         title: "Wardi",
-        urls: ["https://plus.unsplash.com/premium_vector-1682269758143-efffdfc7c67d",
-"https://cdn.logojoy.com/wp-content/uploads/20201117163112/birdland_banner-1536x768.jpg"],
+        urls: [
+            "https://plus.unsplash.com/premium_vector-1682269758143-efffdfc7c67d",
+            "https://cdn.logojoy.com/wp-content/uploads/20201117163112/birdland_banner-1536x768.jpg",
+            "https://cdn.logojoy.com/wp-content/uploads/20201117163031/mississppi_ricks_banner-1-1536x768.jpg",
+            "https://cdn.logojoy.com/wp-content/uploads/20201117162958/glo_banner-1-1536x768.jpg",
+            "https://cdn.logojoy.com/wp-content/uploads/20201021101704/social-cover-2nd-960x1024.jpg",
+        ],
         description: "Versión Web de aplicación existente",
     },
     {
         id: 2,
         title: "Envia Flores",
-        urls: ["https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+        urls: [
+            "https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        ],
         description: "Redseño de calendario web UI/UX",
     },
 
     {
         id: 3,
         title: "Identifica",
-        urls: ["https://miro.medium.com/v2/resize:fit:1400/format:webp/0*li6oPZ3gK_LmybIQ"],
+        urls: [
+            "https://miro.medium.com/v2/resize:fit:1400/format:webp/0*li6oPZ3gK_LmybIQ",
+        ],
         description: "Diseño y creación de editor de credenciales web",
     },
     {
         id: 4,
-        title: ["Barbería Dominó"],
-        urls: ["https://i.etsystatic.com/11282083/r/il/365fd4/3320096601/il_fullxfull.3320096601_f5hx.jpg"],
+        title: "Barbería Dominó",
+        urls: [
+            "https://i.etsystatic.com/11282083/r/il/365fd4/3320096601/il_fullxfull.3320096601_f5hx.jpg",
+        ],
         description: "Diseño de sitio web Barbería Dominó",
     },
     {
         id: 5,
         title: "Portfolio",
-        urls: ["https://assets.justinmind.com/wp-content/uploads/2021/11/vibrant-web-design-portfolio-template.png"],
+        urls: [
+            "https://assets.justinmind.com/wp-content/uploads/2021/11/vibrant-web-design-portfolio-template.png",
+        ],
         description: "Este sitio web",
     },
 ];
