0505ed2e90fbceb4ecc055fc27a199468788186f
Luis Treto <luisjavier.treto@gmail.com>
2024-07-27 19:18:07 -0600

    Created ArrowNav component

---
+++ src/components/Navigation/ArrowNav.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from "vue";
+import { asset } from "../../utils/utils.js";
+
+defineProps(["reverse"]);
+</script>
+<template>
+    <div>
+        <img
+            class="pointer"
+            :class="[reverse ? 'reverse' : '']"
+            :src="asset('/icons/arrow.svg')"
+            alt="arrow navigation"
+            draggable="false"
+        />
+    </div>
+</template>
+<style scoped>
+img {
+    transition: opacity .3s ease-in-out;
+    opacity: .15;
+    &:hover {
+        opacity: 1;
+    }
+    &.reverse {
+        transform: rotate(180deg);
+    }
+}
+</style>
