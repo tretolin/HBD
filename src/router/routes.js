ba5456903f618921c521bea1c0293c4b48d049e5
Luis Treto <luisjavier.treto@gmail.com>
2024-07-27 19:19:04 -0600

    Project detail page created

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,14 @@ import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
 import Contact from "../pages/Contact.vue";
 import NotFound from "../pages/NotFound.vue";
+import ProjectDetail from "../pages/ProjectDetail.vue";
 
 const routes = [
     { path: "/", component: Home, title: "Home" },
     { path: "/home", component: Home, label: "Home" },
     { path: "/about", component: About, label: "About" },
     { path: "/contact", component: Contact, label: "Contact" },
+    { path: "/project/:id", component: ProjectDetail, name:"project", label: "Detail" },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
