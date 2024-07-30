8dfaeff56227b54d16531d27361eae3f35473a9d
Luis Treto <luisjavier.treto@gmail.com>
2024-07-30 13:55:17 -0600

    contact data file

---
+++ src/data/contact.js
@@ -0,0 +1,29 @@
+export const contactData =[
+    {
+        name: "Email",
+        data: "luisjavier.treto@gmail.com",
+        icon: "mail.svg",
+        link: "",
+    },
+    { name: "Phone", data: "52+8341483633", icon: "phone.svg", link: "" },
+    {
+        name: "Linkedin",
+        data: "Luis Treto Profile",
+        icon: "linkedin.svg",
+        link: "https://www.linkedin.com/in/luis-treto-590879108/",
+    },
+    {
+        name: "Resume",
+        data: "Resume",
+        icon: "contact_page.svg",
+        link: "",
+        label: "Download",
+        file: "/Luis-Treto-Resume.pdf",
+    },
+    // {
+    //     name: "Website",
+    //     data: "Personal website",
+    //     icon: "web.svg",
+    //     link: "https://www.linkedin.com/in/luis-treto-590879108/",
+    // },
+]
