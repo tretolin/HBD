35c09072edf1aa48f4564825fa411a6f7f5fc7ad
Luis Treto <luisjavier.treto@gmail.com>
2024-07-31 13:48:05 -0600

    Last changes

--- src/components/Pallete/Pallete.vue
+++ src/components/Pallete/Pallete.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="theme-pallete">
-        <div class="block primary"></div>
         <div class="block secondary"></div>
         <div class="block font"></div>
         <div class="block link"></div>
         <div class="block link active"></div>
+        <!-- <div class="block primary"></div> -->
     </div>
 </template>
 <style type="scss">
